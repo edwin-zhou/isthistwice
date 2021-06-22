6a71c4ad484eae274ad3b976e0d061d2d895c91b
ed99win <edwin99@live.ca>
2021-06-21 21:54:13 -0400

    .78 acc

--- model.js
+++ model.js
@@ -15,7 +15,7 @@ model.add(
 
 model.add(
     tf.layers.conv2d({
-        kernelSize: 5,
+        kernelSize: 7,
         padding: 'same',
         filters: 16,
         activation: 'relu',
@@ -24,9 +24,20 @@ model.add(
 
 model.add(
     tf.layers.conv2d({
-        kernelSize: 3,
+        kernelSize: 5,
         padding: 'same',
         filters: 32,
+        activation: 'relu',
+    })
+)
+
+model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+
+model.add(
+    tf.layers.conv2d({
+        kernelSize: 3,
+        padding: 'same',
+        filters: 64,
         activation: 'relu',
     })
 )
@@ -57,7 +68,7 @@ model.add(tf.layers.dense({
 }));
 
 model.compile({
-    optimizer: tf.train.adam(0.000001),
+    optimizer: tf.train.adam(0.00001),
     loss: 'binaryCrossentropy',
     metrics: ['accuracy'],
 });
